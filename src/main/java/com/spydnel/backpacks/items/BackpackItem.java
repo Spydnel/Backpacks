095b706a76df8c3bd44835bd2025cd2c8f80d959
cake <cakeistastydev@gmail.com>
2025-10-12 09:39:11 +0100

    Right click to equip from hotbar

--- src/main/java/com/spydnel/backpacks/items/BackpackItem.java
+++ src/main/java/com/spydnel/backpacks/items/BackpackItem.java
@@ -7,10 +7,17 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.AgeableListModel;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.Model;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Equipable;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.client.ClientHooks;
 import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
@@ -19,7 +26,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
-public class BackpackItem extends BlockItem {
+public class BackpackItem extends BlockItem implements Equipable {
     public BackpackItem(Block block, Properties properties) {
         super(block, properties);
     }
@@ -28,4 +35,15 @@ public class BackpackItem extends BlockItem {
     public boolean canFitInsideContainerItems() {
         return false;
     }
+
+    @Override
+    public EquipmentSlot getEquipmentSlot() {
+        return EquipmentSlot.CHEST;
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        return this.swapWithEquipmentSlot(this, level, player, hand);
+    }
+
 }
